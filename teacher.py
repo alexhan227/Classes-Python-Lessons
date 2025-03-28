392656e72bc9fd042e4b20ceec4f98e0122ceac6
alexhan227 <alexanderh281@nycstudents.net>
2025-03-28 09:19:14 -0400

    Day 2
    struggle gng

--- teacher.py
+++ teacher.py
@@ -1,2 +1,20 @@
-class Teacher:
-    def questions(): 
+class FlashCards:
+    def __init__(self, question, answer):
+        
+        self.question = question
+        self.answer = answer
+
+    def ask_question(self):
+        print(self.question)
+
+    def the_answer(self):
+        print(self.answer)
+            
+question = input("what is the question?: ")
+answer = input("What is the answer?: ")
+    
+
+FlashCards1 = FlashCards(question, answer)
+
+FlashCards1.ask_question()
+FlashCards1.the_answer()
