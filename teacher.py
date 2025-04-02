33ff40a7d7fe551f34df3d2066562691e1c0a6b7
alexhan227 <alexanderh281@nycstudents.net>
2025-04-02 09:16:33 -0400

    dAY ZIJIE KING

--- teacher.py
+++ teacher.py
@@ -1,30 +1,24 @@
+import json
+Q={}
+p=[]
 class FlashCards:
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
 
-    def ask_question(self):
-        print(self.question)
-
-    def the_answer(self):
-        print(self.answer)
-                
+    def dictionary(self):
+        Q[question]=answer
+              
 question = input("what is the question?: ")
 answer = input("What is the answer?: ")
     
 
 FlashCards1 = FlashCards(question, answer)
-FlashCards1.ask_question()
-FlashCards1.the_answer()
-
-FlashCards2 = FlashCards(question, answer)
-FlashCards1.ask_question()
-FlashCards1.the_answer()
-
-FlashCards3 = FlashCards(question, answer)
-FlashCards1.ask_question()
-FlashCards1.the_answer()
-
-Q = []
+FlashCards1.dictionary()
 
 
+p.append(Q)
+
+# Save updated data back to file
+with open("FlashCards.json", "w") as file:
+    json.dump(p, file, indent=4)
